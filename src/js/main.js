dc087e466849a500c8a1b79a0eee46340d1137fd
ArturCiepielowski <artciepi@wp.pl>
2019-08-04 18:01:24 +0200

    feat: 3rd commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,26 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
+
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
+    
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
+    }, function (err) {
+      
       console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
 
-// place your code below
+
+console.log('https://www.youtube.com/watch?v=eaEMSKzqGAg');
 
 
-console.log(`Hello world!`);
+    document.getElementById("button__loadMore").onclick = function () {
+        location.href = "https://github.com/ArturCiepielowski?tab=repositories";
+    };
+
 
 
